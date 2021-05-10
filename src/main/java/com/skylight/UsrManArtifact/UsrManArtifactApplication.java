ddfefea61f3eba2ce174c2b4828655f00100f07d
dkusumandaru <darmakusumandaru@gmail.com>
2021-05-10 15:21:40 +0700

    init project

---
+++ src/main/java/com/skylight/UsrManArtifact/UsrManArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.skylight.UsrManArtifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsrManArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsrManArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skylight/UsrManArtifact/UsrManArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skylight.UsrManArtifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsrManArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
